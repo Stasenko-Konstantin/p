8cc8849c5742a377db694e011307d5b860a67552
Stasenko-Konstantin <stasenko.kost@yandex.ru>
2023-03-17 12:16:09 +0500

    some doc

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # p
 
-parser-combinators library
+```go
+func satisfy[I, E any](p func(I) bool) Parser[I, E, I] {
+    return func (i []I) Either[[]Error, Tuple2[I, []I]] {
+        if len(i) == 0 {
+            return Either[[]Error, Tuple2[I, []I]]{left: &[]Error{EndOfInput{}}}
+        } else if p(i[0]) {
+            return Either[[]Error, Tuple2[I, []I]]{right: &Tuple2[I, []I]{i[0], i[1:]}}
+        }
+        return Either[[]Error, Tuple2[I, []I]]{left: &[]Error{Unexpected[I]{i[0]}}}
+    }
+}
+
+func char[I comparable, E any](i I) Parser[I, E, I] {
+    return satisfy[I, E](func (i2 I) bool {
+        return i == i2
+    })
+}
+
+func test_char() {
+    r := char('e').RunParser("eas") // Right( Tuple2( 'e', "as" ) )
+}
+
+////////////
+// Either //
+////////////
+
+if err != nil {
+    return p.Left(&err)
+}
+return p.Right(&42)
+
+func handle(e Either[error, int]) int {
+    if _, ok := e.Left(); ok {
+        return 69
+    } else if res, ok := e.Right(); ok {
+        return *res
+    }
+    return 0
+}
+```
--- either.go
+++ either.go
@@ -2,28 +2,53 @@ package p
 
 import "fmt"
 
+/*
+Either - type from functional languages for error handling. Left often is error, Right - result
+
+		if err != nil {
+			return p.Left(&err)
+		}
+		return p.Right(&42)
+
+		func handle(e Either[error, int]) int {
+			if _, ok := e.Left(); ok {
+				return 0
+			} else if res, ok := e.Right(); ok {
+				return *res
+			}
+		}
+
+*/
 type Either[L, R any] struct {
 	left  *L
 	right *R
 }
 
-func (e Either[L, _]) Left() (left *L, ok bool) {
+func Left(left *any) Either[any, any] {
+	return Either[any, any]{left: left}
+}
+
+func Right(right *any) Either[any, any] {
+	return Either[any, any]{right: right}
+}
+
+func (e *Either[L, _]) Left() (left *L, ok bool) {
 	if e.left != nil {
 		return e.left, true
 	}
 	return nil, false
 }
 
-func (e Either[_, R]) Right() (right *R, ok bool) {
+func (e *Either[_, R]) Right() (right *R, ok bool) {
 	if e.right != nil {
 		return e.right, true
 	}
 	return nil, false
 }
 
-func (e Either[L, R]) String() string {
+func (e *Either[L, R]) String() string {
 	var (
-		res any
+		res  any
 		side string
 	)
 	if l, ok := e.Left(); ok {
